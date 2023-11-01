4533bbec494e878f1e9938e248e623c03e513986
joe-egan <144191133+joe-egan@users.noreply.github.com>
2023-11-01 11:21:49 -0500

    Update files.

--- add-commit-pushpython.py
+++ add-commit-pushpython.py
@@ -1,1 +1,12 @@
-print("Add commit Push")
+import os 
+
+print('add-commit-push')
+print('\ngit status')
+os.system('git status')
+
+print('\ngit add -A')
+os.system('git add -A')
+print('\ngit commit -m "Update files."')
+os.system('git commit -m "Update files."')
+print('\ngit push')
+os.system('git push')
