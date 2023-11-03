cc0f0decf1e1c1e65425e958aa58938e15eab290
joe-egan <144191133+joe-egan@users.noreply.github.com>
2023-11-03 11:43:28 -0500

    updatepython file 2

--- add-commit-pushpython.py
+++ add-commit-pushpython.py
@@ -6,11 +6,16 @@ if len(sys.argv) == 3:
     if sys.argv[1] == '-m':
             commitCommand = '\ngit commit -m "' + sys.argv[2] + '"'
 
-print(commitCommand)
-
 print('add-commit-push')
 print('\ngit status')
 os.system('git status')
+
+print('Continue with add,commit,push? (y):')
+userInput = input()
+if userInput != 'y':
+      print('Canceling program')
+      quit()
+
 
 print('\ngit add -A')
 os.system('git add -A')
